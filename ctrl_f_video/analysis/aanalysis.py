0d3e8b7c0be5a4349b93aeeed74e38d033e48a0b
Luxxii <dl94@live.de>
2021-10-30 16:13:22 +0200

    impl

--- ctrl_f_video/analysis/aanalysis.py
+++ ctrl_f_video/analysis/aanalysis.py
@@ -1,14 +1,11 @@
 from abc import ABC, abstractmethod
 
 class Aanalysis(ABC):
-    pass
 
     @abstractmethod(callable)
     def anaylze(obj): # returns search struct
         pass
 
 
-    for i in range(0,10):
-        pass
 
     # Methods for analysis
